22d9b916a510d73790fc16ddb395e0307f1b796b
Nidhi Soni <nidhi.soni@mendix.com>
2023-01-25 16:02:05 +0100

    920

---
+++ deployment/native/styles.js
@@ -0,0 +1,10 @@
+import * as style0 from "//mac/Home/Documents/Mendix/new920app-main/themesource/administration/native/main";
+import * as style1 from "//mac/Home/Documents/Mendix/new920app-main/themesource/nanoflowcommons/native/main";
+import * as style2 from "//mac/Home/Documents/Mendix/new920app-main/themesource/webactions/native/main";
+import * as style3 from "//mac/Home/Documents/Mendix/new920app-main/themesource/atlas_core/native/main";
+import * as style4 from "//mac/Home/Documents/Mendix/new920app-main/themesource/myfirstmodule/native/main";
+import * as style5 from "//mac/Home/Documents/Mendix/new920app-main/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5]);
